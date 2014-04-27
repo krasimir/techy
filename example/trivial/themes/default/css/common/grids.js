949f3e434f656ae35a6df0c13aef827155fd905a
Krasimir Tsonev <krasimir@outset.ws>
2014-04-27 13:58:00 +0300

    Adding favicon to the default theme

--- example/trivial/themes/default/css/common/grids.js
+++ example/trivial/themes/default/css/common/grids.js
@@ -1,11 +1,27 @@
 module.exports = function(api) {
-	var possibleChilds = ['p'];
-	var maxColumns = 6;
-	for(var i=2; i<=maxColumns; i++) {
+	var settings = api.settings.layouts.techy;
+	var grids = [
+		{ breakpoint: 600, cols: 2 },
+		{ breakpoint: 670, cols: 3 },
+		{ breakpoint: 700, cols: 4 }
+	];
+	for(var i=0; i<grids.length; i++) {
 		var styles = {};
-		for(var j=0; j<possibleChilds.length; j++) {
-			styles['.grid-' + i] = { grid: i + '/' + possibleChilds[j] };
-			api.add(styles);
+		styles['.grid-' + grids[i].cols] = { 
+			grid: grids[i].cols + '/.grid-column',
+			'.grid-column': {
+				p: {
+					pl: 0,
+					pd: 0
+				}
+			}
+		};
+		styles['.grid-' + grids[i].cols]['@media all and (max-width: ' + grids[i].breakpoint + 'px)'] = {
+			'.grid-column': {
+				fl: 'n',
+				wid: '100%'
+			}
 		}
+		api.add(styles);
 	}
 }
